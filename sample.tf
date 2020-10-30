7bc6722ca937fa397eeea3aca0ab4a1f80308f43
Charly-1992 <53769542+Charly-1992@users.noreply.github.com>
2020-10-30 13:43:51 +0100

    Update 1 sample.tf
    Adding OS and Flavor

--- sample.tf
+++ sample.tf
@@ -1,13 +1,11 @@
 resource "ibm_compute_vm_instance" "vm1" {
  hostname = "vm1"
  domain = "example.com"
- os_reference_code = "DEBIAN_8_64"
- datacenter = "dal09"
- network_speed = 10
+ os_reference_code = "CENTOS_LATEST"
+ datacenter = "fra02"
+ network_speed = 100
  hourly_billing = true
  private_network_only = false
- cores = 1
- memory = 1024
- disks = [25]
+ flavor_key_name = "M1_1X8X25"
  local_disk = false
 }
