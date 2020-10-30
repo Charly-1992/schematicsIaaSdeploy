08b23602f675222bfa35e755580b2cf661e5e2c4
Charly-1992 <53769542+Charly-1992@users.noreply.github.com>
2020-10-30 13:52:24 +0100

    Create dedicatedserver.tf

---
+++ dedicatedserver.tf
@@ -0,0 +1,11 @@
+resource "ibm_compute_bare_metal" "hourly-bm1" {
+    hostname = "hourly-bm1"
+    domain = "redhat.com"
+    os_reference_code = "CUSTOS_1_64"
+    datacenter = "fra05"
+    network_speed = 100 # Optional
+    hourly_billing = true
+    private_network_only = false # Optional
+    fixed_config_preset = "1U_4210S_384GB_2X4TB_RAID_1"
+    notes = "node test"
+}
