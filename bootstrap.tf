7333d727633d28e4df0eb039cb7280b3707ec672
Charly-1992 <53769542+Charly-1992@users.noreply.github.com>
2020-11-03 18:38:22 +0100

    Update bootstrap.tf
    Fixed_config_preset modified

--- bootstrap.tf
+++ bootstrap.tf
@@ -6,6 +6,6 @@ resource "ibm_compute_bare_metal" "bootstrap-bm" {
     network_speed = 100 # Optional
     hourly_billing = true
     private_network_only = false # Optional
-    fixed_config_preset = "1U_4210S_384GB_2X4TB_RAID_1"
+    fixed_config_preset = "1U_2174S_64GB_2X4TB_RAID_1"
     notes = "node test"
 }
